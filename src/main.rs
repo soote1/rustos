082f86807b08213047ecbdd7bb2190c9c03ea893
Isaias Ramirez <soote1.ir@gmail.com>
2022-10-23 14:43:21 -0500

    Write text to the vga buffer
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static TEXT: &[u8] = b"Hello from bare metal world!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // initialize raw pointer at vga buffer location
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in TEXT.iter().enumerate() {
+        unsafe {
+            // write char byte
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // write color byte (light cyan)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }

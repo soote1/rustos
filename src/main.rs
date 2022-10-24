d693d99bbedda3b6d850c41f56a235712c6ce31d
Isaias Ramirez <soote1.ir@gmail.com>
2022-10-23 23:10:34 -0500

    Implement a module to interact with the vga buffer
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-static TEXT: &[u8] = b"Hello from bare metal world!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // initialize raw pointer at vga buffer location
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in TEXT.iter().enumerate() {
-        unsafe {
-            // write char byte
-            *vga_buffer.offset(i as isize * 2) = byte;
-            // write color byte (light cyan)
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
     loop {}
 }
